5f07e7f0e8f512e5fa74e8ce67001ec6b87c7624
root <root@coral-john.eoli.info>
2021-12-02 08:58:54 -0500

    Replaced .live with .on in remaining files

--- resources/js/forms/attachmentForm.js
+++ resources/js/forms/attachmentForm.js
@@ -31,7 +31,7 @@ $( document ).ready(function() {
 
     	$('.changeInput').addClass("idleField");
 
-	$('.changeInput').live('focus', function() {
+	$('.changeInput').on('focus', function() {
 
 
 		$(this).removeClass("idleField").addClass("focusField");
@@ -43,7 +43,7 @@ $( document ).ready(function() {
 	 });
 
 
-	 $('.changeInput').live('blur', function() {
+	 $('.changeInput').on('blur', function() {
 		$(this).removeClass("focusField").addClass("idleField");
 	 });
 
@@ -51,12 +51,12 @@ $( document ).ready(function() {
 
 
 	$('select').addClass("idleField");
-	$('select').live('focus', function() {
+	$('select').on('focus', function() {
 		$(this).removeClass("idleField").addClass("focusField");
 
 	});
 
-	$('select').live('blur', function() {
+	$('select').on('blur', function() {
 		$(this).removeClass("focusField").addClass("idleField");
 	});
 
